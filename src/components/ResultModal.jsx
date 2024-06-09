2ad6ad974fa269411b77ab4994771694ae2cdae6
Vivan-verma1109 <vivanv732@gmail.com>
2024-06-09 15:47:13 -0700

    adding files

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,44 @@
+import React, { forwardRef, useRef, useImperativeHandle } from "react";
+import { createPortal } from "react-dom";
+
+const ResultModal = forwardRef(function ResultModal(
+  { targetTime, remainingTime, onReset },
+  ref
+) {
+  const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
+  useImperativeHandle(ref, () => ({
+    open() {
+      dialog.current.showModal();
+    },
+    close() {
+      dialog.current.close();
+    },
+  }));
+
+  return createPortal(
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2> You Lost!</h2>}
+      {!userLost && <h2>Your score: {score} </h2>}
+      <p>
+        The target time was <strong>{targetTime} seconds</strong>
+      </p>
+      <p>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left</strong>
+      </p>
+      <form method="dialog" onSubmit={onReset}>
+        <button type="submit">Close</button>
+      </form>
+    </dialog>,
+    document.getElementById("modal")
+  );
+});
+
+export default ResultModal;
